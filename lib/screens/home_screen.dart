24d1e94cce0291466baf85e0c866295a565d5d6c
Faridah Faadhilah <dhila@Faridahs-MacBook-Air.local>
2024-11-19 15:32:07 +0700

    Use CollapsibleAppBar at home page

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -22,54 +22,66 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Awesome App'),
-        actions: [
-          IconButton(
-            icon: Icon(_isGridView ? Icons.view_list : Icons.grid_view),
-            onPressed: () => setState(() {
-              _isGridView = !_isGridView;
-            }),
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            expandedHeight: 200.0,
+            floating: false,
+            pinned: true,
+            flexibleSpace: FlexibleSpaceBar(
+              title: Text('Awesome App'),
+              background: Image.network(
+                'https://source.unsplash.com/random',
+                fit: BoxFit.cover,
+              ),
+            ),
+            actions: [
+              IconButton(
+                icon: Icon(_isGridView ? Icons.view_list : Icons.grid_view),
+                onPressed: () => setState(() {
+                  _isGridView = !_isGridView;
+                }),
+              ),
+            ],
+          ),
+          BlocBuilder<PhotoBloc, PhotoState>(
+            builder: (context, state) {
+              if (state is PhotoLoading) {
+                return SliverFillRemaining(
+                  child: Center(child: CircularProgressIndicator()),
+                );
+              } else if (state is PhotoLoaded) {
+                final photos = state.photos;
+                return _isGridView
+                    ? SliverGrid(
+                        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 2,
+                          crossAxisSpacing: 8,
+                          mainAxisSpacing: 8,
+                        ),
+                        delegate: SliverChildBuilderDelegate(
+                          (context, index) {
+                            return PhotoGridItem(photo: photos[index]);
+                          },
+                          childCount: photos.length,
+                        ),
+                      )
+                    : SliverList(
+                        delegate: SliverChildBuilderDelegate(
+                          (context, index) {
+                            return PhotoListItem(photo: photos[index]);
+                          },
+                          childCount: photos.length,
+                        ),
+                      );
+              } else {
+                return SliverFillRemaining(
+                  child: Center(child: Text('Failed to load photos')),
+                );
+              }
+            },
           ),
         ],
-      ),
-      body: BlocBuilder<PhotoBloc, PhotoState>(
-        builder: (context, state) {
-          if (state is PhotoLoading) {
-            return Center(child: CircularProgressIndicator());
-          } else if (state is PhotoLoaded) {
-            final photos = state.photos;
-            return NotificationListener<ScrollNotification>(
-              onNotification: (scrollNotification) {
-                if (scrollNotification.metrics.pixels ==
-                    scrollNotification.metrics.maxScrollExtent) {
-                  context.read<PhotoBloc>().add(LoadMorePhotos());
-                }
-                return false;
-              },
-              child: _isGridView
-                  ? GridView.builder(
-                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                        crossAxisCount: 2,
-                        crossAxisSpacing: 8,
-                        mainAxisSpacing: 8,
-                      ),
-                      itemCount: photos.length,
-                      itemBuilder: (context, index) {
-                        return PhotoGridItem(photo: photos[index]);
-                      },
-                    )
-                  : ListView.builder(
-                      itemCount: photos.length,
-                      itemBuilder: (context, index) {
-                        return PhotoListItem(photo: photos[index]);
-                      },
-                    ),
-            );
-          } else {
-            return Center(child: Text('Failed to load photos'));
-          }
-        },
       ),
     );
   }
